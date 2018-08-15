b45a43aa7c779d369db8a1a13213a088e29129f0
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-08-15 20:33:01 +0200

    mutable name, description and List of genres
    the name and description are now mutable. therefore setters were added. the constructor and the setters better assure the validity of the parameters. genres are now saved in an List which can be extended by additional entries. toString(), hashCode(), and equals(Object) were changed accordingly.

--- src/de/nuttercode/androidprojectss2018/csi/Event.java
+++ src/de/nuttercode/androidprojectss2018/csi/Event.java
@@ -1,7 +1,9 @@
 package de.nuttercode.androidprojectss2018.csi;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * POJO for an Event entry in the database
@@ -13,22 +15,45 @@ public class Event implements Serializable {
 
 	private static final long serialVersionUID = -6794608442641090075L;
 
-	private final Genre[] genres;
-	private final String description;
-	private final String name;
+	private final List<Genre> genreList;
+	private String description;
+	private String name;
 	private final int id;
 	private final Venue venue;
 
-	public Event(Venue venue, Genre[] genres, String name, String description, int id) {
-		this.genres = genres;
-		this.name = name;
-		this.description = description;
+	/**
+	 * 
+	 * @param venue
+	 * @param genreCollection
+	 * @param name
+	 * @param description
+	 * @param id
+	 * @throws IllegalArgumentException
+	 *             if name is null or empty, if description is null, if
+	 *             genreCollection is null, or if venue is null
+	 */
+	public Event(Venue venue, Collection<Genre> genreCollection, String name, String description, int id) {
+		Assurance.assureNotNull(venue);
+		Assurance.assureNotNull(genreCollection);
+		this.genreList = new ArrayList<>(genreCollection);
 		this.id = id;
 		this.venue = venue;
+		setName(name);
+		setDescription(description);
 	}
 
-	public Genre[] getGenres() {
-		return genres;
+	public List<Genre> getGenres() {
+		return new ArrayList<>(genreList);
+	}
+
+	/**
+	 * @param genre
+	 * @throws IllegalArgumentException
+	 *             if genre is null
+	 */
+	public void addGenre(Genre genre) {
+		Assurance.assureNotNull(genre);
+		genreList.add(genre);
 	}
 
 	public String getDescription() {
@@ -47,12 +72,32 @@ public class Event implements Serializable {
 		return venue;
 	}
 
+	/**
+	 * @param name
+	 * @throws IllegalArgumentException
+	 *             if name is null or empty
+	 */
+	public void setName(String name) {
+		Assurance.assureNotEmpty(name);
+		this.name = name;
+	}
+
+	/**
+	 * @param description
+	 * @throws IllegalArgumentException
+	 *             if description is null
+	 */
+	public void setDescription(String description) {
+		Assurance.assureNotNull(description);
+		this.description = description;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + Arrays.hashCode(genres);
+		result = prime * result + ((genreList == null) ? 0 : genreList.hashCode());
 		result = prime * result + id;
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((venue == null) ? 0 : venue.hashCode());
@@ -73,7 +118,10 @@ public class Event implements Serializable {
 				return false;
 		} else if (!description.equals(other.description))
 			return false;
-		if (!Arrays.equals(genres, other.genres))
+		if (genreList == null) {
+			if (other.genreList != null)
+				return false;
+		} else if (!genreList.equals(other.genreList))
 			return false;
 		if (id != other.id)
 			return false;
@@ -92,8 +140,8 @@ public class Event implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Event [genres=" + Arrays.toString(genres) + ", description=" + description + ", name=" + name + ", id="
-				+ id + ", venue=" + venue + "]";
+		return "Event [genreList=" + genreList + ", description=" + description + ", name=" + name + ", id=" + id
+				+ ", venue=" + venue + "]";
 	}
 
 }
