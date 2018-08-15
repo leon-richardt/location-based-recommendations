80e27c8ae60b43560e1ccf4d2c0575c27aa914d3
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-08-15 20:34:26 +0200

    better documentation, constrained constructor
    the constructor now enforces scores in the range of [0, 1]

--- src/de/nuttercode/androidprojectss2018/csi/ScoredEvent.java
+++ src/de/nuttercode/androidprojectss2018/csi/ScoredEvent.java
@@ -3,7 +3,7 @@ package de.nuttercode.androidprojectss2018.csi;
 import java.io.Serializable;
 
 /**
- * {@link Event} + score
+ * an {@link Event} and its score. the score must be an element of [0, 1].
  * 
  * @author Johannes B. Latzel
  *
@@ -15,7 +15,15 @@ public class ScoredEvent implements Serializable {
 	private final Event event;
 	private final double score;
 
+	/**
+	 * @param event
+	 * @param score
+	 * @throws IllegalArgumentException
+	 *             if event == null or score not an element of [0, 1]
+	 */
 	public ScoredEvent(Event event, double score) {
+		Assurance.assureNotNull(event);
+		Assurance.assureBoundaries(score, 0, 1);
 		this.event = event;
 		this.score = score;
 	}
