14116cb2d9a19dc3a523d8490e0ffb897525df79
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-08-22 14:03:46 +0200

    iterable implementiert und containsAny hinzugefügt
    iterable implementiert und containsAny hinzugefügt

--- src/de/nuttercode/androidprojectss2018/csi/GenrePreferenceConfiguration.java
+++ src/de/nuttercode/androidprojectss2018/csi/GenrePreferenceConfiguration.java
@@ -2,7 +2,9 @@ package de.nuttercode.androidprojectss2018.csi;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -11,7 +13,7 @@ import java.util.Set;
  * @author Johannes B. Latzel
  *
  */
-public class GenrePreferenceConfiguration implements Serializable {
+public class GenrePreferenceConfiguration implements Serializable, Iterable<Genre> {
 
 	private static final long serialVersionUID = 4061390806054215254L;
 
@@ -31,10 +33,26 @@ public class GenrePreferenceConfiguration implements Serializable {
 		userChoiceGenreSet.add(genre);
 	}
 
+	/**
+	 * @param genres
+	 * @return true if any configured genre is contained in the given Collection
+	 */
+	public boolean containsAny(Collection<Genre> genres) {
+		for (Genre genre : this)
+			if (genres.contains(genre))
+				return true;
+		return false;
+	}
+
 	@Override
 	public String toString() {
 		return "GenrePreferenceConfiguration [userChoiceGenreSet=" + Arrays.toString(userChoiceGenreSet.toArray())
 				+ "]";
+	}
+
+	@Override
+	public Iterator<Genre> iterator() {
+		return userChoiceGenreSet.iterator();
 	}
 
 }
