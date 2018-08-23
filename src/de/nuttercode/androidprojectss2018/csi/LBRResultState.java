cb34ef6cbbf419a91196aeda5782358608e2e934
leon-richardt <leon.richardt@gmail.com>
2018-08-23 13:52:51 +0200

    Changed .gitignore .gitignore does no longer contain the .gradle diretory or *.iml files

--- src/de/nuttercode/androidprojectss2018/csi/LBRResultState.java
+++
@@ -1,5 +0,0 @@
-package de.nuttercode.androidprojectss2018.csi;
-
-public enum LBRResultState {
-	OK, ClassNotFoundException, UnknownHostException, IOException, RuntimeException, InterruptedException, Null
-}
