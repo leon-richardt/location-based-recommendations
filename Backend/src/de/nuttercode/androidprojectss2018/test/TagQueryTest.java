e58151f7eed4c21546f18a10436e29bec68df555
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-08-27 14:42:58 +0200

    Test-Klassen an neue Klassen angepasst
    Test-Klassen an neue Klassen angepasst

--- Backend/src/de/nuttercode/androidprojectss2018/test/TagQueryTest.java
+++ Backend/src/de/nuttercode/androidprojectss2018/test/TagQueryTest.java
@@ -2,6 +2,7 @@ package de.nuttercode.androidprojectss2018.test;
 
 import de.nuttercode.androidprojectss2018.csi.ClientConfiguration;
 import de.nuttercode.androidprojectss2018.csi.TagStore;
+import de.nuttercode.androidprojectss2018.example.StoreListenerExample;
 
 /**
  * Test LBRClient connection
@@ -14,11 +15,10 @@ public class TagQueryTest {
 	public static void main(String[] args) {
 
 		ClientConfiguration clientConfiguration = new ClientConfiguration();
-		clientConfiguration.setServerDNSName("localhost");
 		TagStore tagStore = new TagStore(clientConfiguration);
-		System.out.println(tagStore);
-		System.out.println(tagStore.refreshTags());
-		System.out.println(tagStore);
+		clientConfiguration.setServerDNSName("localhost");
+		tagStore.addStoreListener(new StoreListenerExample<>());
+		System.out.println(tagStore.refresh());
 
 	}
 
