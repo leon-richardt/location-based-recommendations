0efb022856264c76f75cc554b14e1249d1ed1c2c
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-08-27 14:41:43 +0200

    hashcode angepasst
    hashcode ist nur noch die id, weil schneller (speziell bei Nutzung von HashSet)

--- Backend/src/de/nuttercode/androidprojectss2018/csi/Venue.java
+++ Backend/src/de/nuttercode/androidprojectss2018/csi/Venue.java
@@ -82,19 +82,7 @@ public class Venue implements Serializable {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + id;
-		long temp;
-		temp = Double.doubleToLongBits(initialDistance);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(latitude);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(longitude);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+		return id;
 	}
 
 	@Override
