55bbb584ec95db4416614f3a6f6c973781e6028f
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-08-28 18:45:11 +0200

    neue Klasse für DB-Query-Ausführung
    neue Klasse für DB-Query-Ausführung

---
+++ Backend/src/de/nuttercode/androidprojectss2018/db/DoQuery.java
@@ -0,0 +1,139 @@
+package de.nuttercode.androidprojectss2018.db;
+
+import de.nuttercode.androidprojectss2018.csi.Assurance;
+import de.nuttercode.androidprojectss2018.csi.Event;
+import de.nuttercode.androidprojectss2018.csi.Tag;
+import de.nuttercode.androidprojectss2018.csi.Venue;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ * provides static access to MySQL DB for LBR-Instances
+ * 
+ * @author Henrik Gerdes, Johannes B. Latzel
+ *
+ */
+public class DoQuery {
+
+	private static Connection conn = null;
+	private static Statement stm = null;
+
+	/**
+	 * Initializes the DB Connection
+	 * 
+	 * @param url
+	 *            The DB URL, Port and DB-Name
+	 * @param user
+	 *            Username
+	 * @param password
+	 *            Password for Username
+	 * @throws SQLException
+	 *             If login is not successful
+	 */
+	public static void GetConnection(String url, String user, String password) throws SQLException {
+		conn = DriverManager.getConnection(url, user, password);
+		stm = conn.createStatement();
+
+	}
+
+	/**
+	 * Gets all Events for the given query
+	 * 
+	 * @param query
+	 *            The query that specifics the Events
+	 * @return ArrayList of Events
+	 * @throws SQLException
+	 *             SQL Error
+	 * @throws IllegalStateException
+	 *             if {@link #CheckConnection()} does
+	 */
+	public static ArrayList<Event> GetEvents(String query) throws SQLException {
+		CheckConnection();
+		ArrayList<Event> events = new ArrayList<>();
+		ResultSet dbResult = stm.executeQuery(query);
+		while (dbResult.next())
+			events.add(new Event(new Venue(dbResult.getInt("venue_id"), dbResult.getString("venue_name"),
+					dbResult.getString("venue_description"), dbResult.getBigDecimal("longitude").doubleValue(),
+					dbResult.getBigDecimal("latitude").doubleValue(), dbResult.getBigDecimal("distance").doubleValue()),
+					dbResult.getString("event_name"), dbResult.getString("event_description"),
+					dbResult.getInt("event_id"), getLocalDateTime(dbResult.getDate("date"), dbResult.getTime("time"))));
+		return events;
+	}
+
+	/**
+	 * provides default values for nullable date and time
+	 * 
+	 * @param date
+	 * @param time
+	 * @return LocalDateTime with default values if parameters are null
+	 */
+	private static LocalDateTime getLocalDateTime(Date date, Time time) {
+		Date actualDate = date;
+		if (actualDate == null)
+			actualDate = new Date(0);
+		Time actualTime = time;
+		if (actualTime == null)
+			actualTime = new Time(0);
+		return LocalDateTime.of(actualDate.toLocalDate(), actualTime.toLocalTime());
+	}
+
+	/**
+	 * Gets the Tags for every Event
+	 * 
+	 * @param events
+	 *            List of Events
+	 * @return ArrayList of Events with Tags
+	 * @throws SQLException
+	 *             SQL error
+	 * @throws IllegalArgumentException
+	 *             if events is null or it contains null
+	 * @throws IllegalStateException
+	 *             if {@link #CheckConnection()} does
+	 */
+	public static ArrayList<Event> GetEventsWithTag(ArrayList<Event> events) throws SQLException {
+		CheckConnection();
+		Assurance.assureNotNull(events);
+		for (Event e : events) {
+			Assurance.assureNotNull(e);
+			ResultSet dbResult = stm.executeQuery(GenerateQuery.GenerateQueryTagsForEvent(e));
+			while (dbResult.next())
+				e.addTag(new Tag(dbResult.getInt("tag_id"), dbResult.getString("tag_name"),
+						dbResult.getString("tag_description")));
+		}
+		return events;
+	}
+
+	/**
+	 * Gets all Tags
+	 * 
+	 * @return ArrayList of all Tags
+	 * @throws SQLException
+	 *             SQL error
+	 */
+	public static ArrayList<Tag> GetAllTags() throws SQLException {
+		CheckConnection();
+		ResultSet dbResult = stm.executeQuery(GenerateQuery.GenerateQueryForAllTags());
+		ArrayList<Tag> tags = new ArrayList<>();
+		while (dbResult.next())
+			tags.add(new Tag(dbResult.getInt("tag_id"), dbResult.getString("tag_name"),
+					dbResult.getString("tag_description")));
+		return tags;
+	}
+
+	/**
+	 * Checks if the Connection was initialized before working on query
+	 */
+	private static void CheckConnection() {
+		if (conn == null || stm == null) {
+			throw new IllegalStateException("Please initialize Connection first ");
+		}
+	}
+}
