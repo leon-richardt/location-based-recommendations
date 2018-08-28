3584ff6623f20ba6c472e753568179c77e801b8b
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-08-28 18:44:26 +0200

    neue Test-Klasse
    neue Test-Klasse

---
+++ Backend/src/de/nuttercode/androidprojectss2018/test/DBConnectionTest.java
@@ -0,0 +1,35 @@
+package de.nuttercode.androidprojectss2018.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
+import de.nuttercode.androidprojectss2018.csi.Event;
+import de.nuttercode.androidprojectss2018.csi.Tag;
+import de.nuttercode.androidprojectss2018.db.DBConnection;
+
+/**
+ * Test-class of {@link DBConnection}
+ * 
+ * @author Johannes B. Latzel
+ *
+ */
+public class DBConnectionTest {
+
+	public static void main(String[] args) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			DBConnection dbConnection = new DBConnection("jdbc:mysql://lbr.nuttercode.de:3306/lbr", "lbr",
+					reader.readLine());
+			for (Tag tag : dbConnection.getAllTags())
+				System.out.println(tag);
+			for (Event event : dbConnection.getAllEventsByRadiusAndLocation(10000, 52.2951671, 8.045340530992085))
+				System.out.println(event);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
