d23e2275c5924b02b8bc4a2a07f4b4d6245d15ee
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2018-09-06 14:28:50 +0200

    try-with-resource for dbconnection
    try-with-resource for dbconnection

--- Backend/src/de/nuttercode/androidprojectss2018/test/DBConnectionTest.java
+++ Backend/src/de/nuttercode/androidprojectss2018/test/DBConnectionTest.java
@@ -20,12 +20,13 @@ public class DBConnectionTest {
 	public static void main(String[] args) {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
 			System.out.print("enter password: ");
-			DBConnection dbConnection = new DBConnection("jdbc:mysql://lbr.nuttercode.de:3306/lbr", "lbr",
-					reader.readLine());
-			for (Tag tag : dbConnection.getAllTags())
-				System.out.println(tag);
-			for (Event event : dbConnection.getAllEventsByRadiusAndLocation(10, 52.3, 8.05))
-				System.out.println(event);
+			try (DBConnection dbConnection = new DBConnection("jdbc:mysql://lbr.nuttercode.de:3306/lbr", "lbr",
+					reader.readLine())) {
+				for (Tag tag : dbConnection.getAllTags())
+					System.out.println(tag);
+				for (Event event : dbConnection.getAllEventsByRadiusAndLocation(10, 52.3, 8.05))
+					System.out.println(event);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (IOException e1) {
