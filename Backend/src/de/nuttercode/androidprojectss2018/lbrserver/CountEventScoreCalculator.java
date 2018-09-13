4358c6f1157a0549270eafcce843ddafe41bff2e
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2018-09-13 17:14:04 +0200

    formula did not satisfy ScoredEvent
    formula did not satisfy ScoredEvent

--- Backend/src/de/nuttercode/androidprojectss2018/lbrserver/CountEventScoreCalculator.java
+++ Backend/src/de/nuttercode/androidprojectss2018/lbrserver/CountEventScoreCalculator.java
@@ -12,9 +12,10 @@ import de.nuttercode.androidprojectss2018.csi.TagUserChoice;
 /**
  * uses the following metric to calculate a score. For every tag in the event a
  * {@link TagUserChoice#Deny} counts as -1, a {@link TagUserChoice#Accept}
- * counts as 0, and a {@link TagUserChoice#Like} counts as +1. All counts are
- * summed up and divided by the total number of {@link Tag}s in the
- * {@link Event}.
+ * counts as 0, and a {@link TagUserChoice#Like} counts as +1. Let m be the
+ * number of +1, n number of -1, and p the number of {@link Tag}s in the
+ * {@link Event}. The score s will be s = (m + n + p) / 2p which satisfies
+ * {@link ScoredEvent}.
  * 
  * @author Johannes B. Latzel
  *
@@ -42,8 +43,11 @@ public class CountEventScoreCalculator implements EventScoreCalculator {
 				break;
 			}
 		}
-		if (!tagList.isEmpty())
-			score = countScore / tagList.size();
+		if (!tagList.isEmpty()) {
+			int size = tagList.size();
+			// double cast to mitigate int-division
+			score = countScore + size / (double) size;
+		}
 		return new ScoredEvent(event, score);
 	}
 
