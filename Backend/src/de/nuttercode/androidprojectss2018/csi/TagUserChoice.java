d6d454165e533f4f961c4da68bdc7514030f8c4e
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2018-09-13 19:40:27 +0200

    added docs

--- Backend/src/de/nuttercode/androidprojectss2018/csi/TagUserChoice.java
+++ Backend/src/de/nuttercode/androidprojectss2018/csi/TagUserChoice.java
@@ -1,5 +1,11 @@
 package de.nuttercode.androidprojectss2018.csi;
 
+/**
+ * possible choice of user for a {@link Tag} in {@link TagPreferenceConfiguration}
+ * 
+ * @author Johannes B. Latzel
+ *
+ */
 public enum TagUserChoice {
 	
 	Deny,
