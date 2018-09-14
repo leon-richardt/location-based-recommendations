a3004e9caf642a8e4f13b1b8175f6827b4531f7c
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2018-09-14 11:35:02 +0200

    base class for POJOs
    base class for POJOs

--- Backend/src/de/nuttercode/androidprojectss2018/csi/pojo/LBRPOJO.java
+++ Backend/src/de/nuttercode/androidprojectss2018/csi/pojo/LBRPOJO.java
@@ -1,11 +1,19 @@
 package de.nuttercode.androidprojectss2018.csi.pojo;
 
-public abstract class LBRPOJO {
+import java.io.Serializable;
+
+public abstract class LBRPOJO implements Serializable {
+	
+	private static final long serialVersionUID = 2162139559240206678L;
 	
 	private final int id;
 	
 	protected LBRPOJO(int id) {
 		this.id = id;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 }
