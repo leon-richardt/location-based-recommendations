f89b1ac160826e6c1909e5a525452268c2680906
leon-richardt <leon.richardt@gmail.com>
2018-09-20 16:25:49 +0200

    Removed unused import

--- Frontend/app/src/main/java/de/nuttercode/androidprojectss2018/app/MapActivity.kt
+++ Frontend/app/src/main/java/de/nuttercode/androidprojectss2018/app/MapActivity.kt
@@ -15,7 +15,6 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
